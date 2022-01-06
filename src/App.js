b9506c18ec8747c19ee3bc9a275d719ab12466a3
unknown <billaboy3397@gmail.com>
2022-01-06 17:00:25 +0800

    Second Changes

--- src/App.js
+++ src/App.js
@@ -4,107 +4,17 @@ import Contact from './components/Contact/contact';
 import Skills from './components/Skills/skills';
 import Projects from './components/Projects/projects';
 import Footer from './components/Footer/footer';
-import Particles from "react-tsparticles";
 
 import Header from './components/Header/header';
+import ParticleBackground from './components/particlesBackground';
 
 
 function App() {
+  
   return (
     
     <div className="app">
-      <Particles options={
-        {
-          "background": {
-            "color": {
-              "value": "#000000"
-            },
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
-          },
-          "fullScreen": {
-            "zIndex": 0
-          },
-          "interactivity": {
-            "events": {
-              "onClick": {
-                "mode": "push"
-              },
-              "onHover": {
-                "mode": "repulse"
-              }
-            },
-            "modes": {
-              "bubble": {
-                "distance": 400,
-                "duration": 2,
-                "opacity": 0.8,
-                "size": 40
-              },
-              "grab": {
-                "distance": 400
-              }
-            }
-          },
-          "particles": {
-            "color": {
-              "value": "#ffffff"
-            },
-            "links": {
-              "color": {
-                "value": "#ffffff"
-              },
-              "distance": 150,
-              "enable": true,
-              "warp": true
-            },
-            "move": {
-              "attract": {
-                "rotate": {
-                  "x": 600,
-                  "y": 1200
-                }
-              },
-              "enable": true,
-              "outModes": {
-                "bottom": "out",
-                "left": "out",
-                "right": "out",
-                "top": "out"
-              },
-              "speed": 6,
-              "warp": true
-            },
-            "number": {
-              "density": {
-                "enable": true
-              },
-              "value": 80
-            },
-            "opacity": {
-              "value": 0.5,
-              "animation": {
-                "speed": 3,
-                "minimumValue": 0.1
-              }
-            },
-            "size": {
-              "random": {
-                "enable": true
-              },
-              "value": {
-                "min": 1,
-                "max": 3
-              },
-              "animation": {
-                "speed": 20,
-                "minimumValue": 0.1
-              }
-            }
-          }
-        }
-      }/>
+      <ParticleBackground/>
       <Header />
       <main>
         <About/>

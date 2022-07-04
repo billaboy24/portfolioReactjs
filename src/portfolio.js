55305c69bcb7c07be3d26e3a8783830386ee2c7b
kathirvan24 <kathirvan@oceannexus.my>
2022-07-04 17:34:46 +0800

    fix-Update skils

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,96 +1,88 @@
 const header = {
-   
-    homepage: 'https://kdevportfolio.netlify.app',
-    title: 'KD.',
-  }
-  
-  const about = {
-   
-    name: 'Kathirvan Pramasivam',
-    role: 'Front End Devoloper',
+  homepage: "https://kdevportfolio.netlify.app",
+  title: "KD.",
+};
+
+const about = {
+  name: "Kathirvan Pramasivam",
+  role: "Front End Devoloper",
+  description:
+    "Let me keep it simple. Once upon time,coding was something I was forced to learn in order to pass my subject. But now,it has become a part of my life. Passionate about software developments and coding and keen to be always up to date about current and upcoming technologies. Currently,pursuing the career path of Full Stack Developer. Starting my career in Front-End Development before jump into Back-end Development and the reason because I believe that if you do not understand the frontend,you will never understand the backend. I have good fundamentals knowledge of Programming and Software Development. Scroll down to know more about it. ",
+  resume:
+    "https://drive.google.com/file/d/1Fz_1Ank1HKBS3bDGJsZ_-BdszNppeKaz/view?usp=sharing",
+  social: {
+    linkedin: "https://www.linkedin.com/in/kathirvanpramasivam/",
+    github: "https://github.com/billaboy24",
+  },
+};
+
+const projects = [
+  {
+    name: "Robofriends",
+    description: "My first ReactJS website that have search function",
+    stack: ["CSS", "HTML", "React"],
+    sourceCode: "https://github.com/billaboy24/robofriendReactjs",
+    livePreview: "https://robofriendsreactjs.netlify.app",
+  },
+  {
+    name: "K-Cloud",
     description:
-      'Let me keep it simple. Once upon time,coding was something I was forced to learn in order to pass my subject. But now,it has become a part of my life. Passionate about software developments and coding and keen to be always up to date about current and upcoming technologies. Currently,pursuing the career path of Full Stack Developer. Starting my career in Front-End Development before jump into Back-end Development and the reason because I believe that if you do not understand the frontend,you will never understand the backend. I have good fundamentals knowledge of Programming and Software Development. Scroll down to know more about it. ',
-    resume: 'https://drive.google.com/file/d/1Fz_1Ank1HKBS3bDGJsZ_-BdszNppeKaz/view?usp=sharing',
-    social: {
-      linkedin: 'https://www.linkedin.com/in/kathirvanpramasivam/',
-      github: 'https://github.com/billaboy24',
-    },
-  }
-  
-  const projects = [
-   
-    {
-      name: 'Robofriends',
-      description:
-        'My first ReactJS website that have search function',
-      stack: ['CSS', 'HTML', 'React'],
-      sourceCode: 'https://github.com/billaboy24/robofriendReactjs',
-      livePreview: 'https://robofriendsreactjs.netlify.app',
-    },
-    {
-      name: 'K-Cloud',
-      description:
-        'Cloud hosting website.This website is prior to my understanding of HTML and CSS and demonstrate my Frontend-Skils.',
-      stack: ['HTML', 'CSS', 'JavaScript'],
-      sourceCode: 'https://github.com/billaboy24/K-Cloud',
-      livePreview: 'https://kcloud.netlify.app',
-    },
-    {
-      name: 'SimpleRecipe',
-      description:
-        'A simple recipe website to demonstrate my frontend skils and boost my understanding in Frontend devlopment',
-      stack: ['CSS', 'HTML', 'JavaScript'],
-      sourceCode: 'https://github.com/billaboy24/simpleRecipe',
-      livePreview: 'https://simplefood.netlify.app',
-    },
-{
-    name: 'MoviesReactJS',
+      "Cloud hosting website.This website is prior to my understanding of HTML and CSS and demonstrate my Frontend-Skils.",
+    stack: ["HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/billaboy24/K-Cloud",
+    livePreview: "https://kcloud.netlify.app",
+  },
+  {
+    name: "SimpleRecipe",
     description:
-      'A  complete react js website that consist of CRUD,,Sorting and etc.This website is prior to my understanding in React JS',
-    stack: ['CSS', 'HTML', 'React','Bootstrap'],
-    sourceCode: 'https://github.com/billaboy24/movieReactjs',
-    livePreview: 'https://moviesreactjs.netlify.app',
+      "A simple recipe website to demonstrate my frontend skils and boost my understanding in Frontend devlopment",
+    stack: ["CSS", "HTML", "JavaScript"],
+    sourceCode: "https://github.com/billaboy24/simpleRecipe",
+    livePreview: "https://simplefood.netlify.app",
+  },
+  {
+    name: "MoviesReactJS",
+    description:
+      "A  complete react js website that consist of CRUD,,Sorting and etc.This website is prior to my understanding in React JS",
+    stack: ["CSS", "HTML", "React", "Bootstrap"],
+    sourceCode: "https://github.com/billaboy24/movieReactjs",
+    livePreview: "https://moviesreactjs.netlify.app",
   },
 
   {
-    name: 'FoodReviewAggregator',
+    name: "FoodReviewAggregator",
     description:
-      'A food review aggregator application that aggregates review from various website.Webscraping was used to obtain review from various websites.',
-    stack: ['CSS', 'React Native','WebScrapping','Firebase'],
-    sourceCode: 'https://github.com/billaboy24/FoodReviewAggregatorApplicationPrototype',
-    livePreview: 'https://github.com/billaboy24/FoodReviewAggregatorApplicationPrototype',
+      "A food review aggregator application that aggregates review from various website.Webscraping was used to obtain review from various websites.",
+    stack: ["CSS", "React Native", "WebScrapping", "Firebase"],
+    sourceCode:
+      "https://github.com/billaboy24/FoodReviewAggregatorApplicationPrototype",
+    livePreview:
+      "https://github.com/billaboy24/FoodReviewAggregatorApplicationPrototype",
   },
   {
-    name: 'Portfolio',
+    name: "Portfolio",
     description:
-      'A simple portfolio website created with React library and Material UI',
-    stack: ['CSS', 'React','HTML','Material UI'],
-    sourceCode: 'https://github.com/billaboy24/portfolioReactjs',
-    livePreview: 'https://kdevportfolio.netlify.app',
+      "A simple portfolio website created with React library and Material UI",
+    stack: ["CSS", "React", "HTML", "Material UI"],
+    sourceCode: "https://github.com/billaboy24/portfolioReactjs",
+    livePreview: "https://kdevportfolio.netlify.app",
   },
-  ]
-  
-  const skills = [
-  
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'Java',
-    'C',
-    'Swift',
-    'React',
-    'React Native',
-    'Bootstrap',
-    'Material UI',
-    'Git',
-    'Adobe XD',
-   
-  ]
-  
-  const contact = {
- 
-    email: 'kathirvandev@gmail.com',
-  }
-  
-  export { header, about, projects, skills, contact }
-  
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Python",
+  "C",
+  "React",
+  "Git",
+  "Docker",
+  "Django",
+];
+
+const contact = {
+  email: "kathirvandev@gmail.com",
+};
+
+export { header, about, projects, skills, contact };
